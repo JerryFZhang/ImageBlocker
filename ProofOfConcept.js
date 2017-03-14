7477e102c60818263660815c9f53cb1d76a3d893
Fengwei Jerry Zhang <jerry.fengwei@gmail.com>
2017-03-14 11:25:44 -0400

    Self Load jQuery, remove all background
    Tested on Bloomberg, Twitter and Facebook

--- ProofOfConcept.js
+++ ProofOfConcept.js
@@ -1,8 +1,19 @@
+(function(){
+  var newscript = document.createElement('script');
+     newscript.type = 'text/javascript';
+     newscript.async = true;
+     newscript.src = 'http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+})();
 setInterval(function() {
 $('img').removeAttr('src');
 $('video').removeAttr("src");
 $('img').removeAttr('data-src');
-$('.tcu-imageWrapper').removeAttr('data-style');
-$('.tcu-imageWrapper').removeAttr('style');
+$('img').removeAttr('srcset');
+//$('.tcu-imageWrapper').removeAttr('data-style');
+//$('.tcu-imageWrapper').removeAttr('style');
+//$('.LiveVideoHomePageModule-postSlate').removeAttr('style');
+//$('.PlayableMedia-player*').removeAttr('style');
+$('*').css("background-image", "");
 console.log("done!");
 }, 100);
